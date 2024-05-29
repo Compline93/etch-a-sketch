dab041eab201a2175b59d0af1ffeeed4784f6052
Compline93 <97375682+Compline93@users.noreply.github.com>
2024-05-29 09:31:28 -0400

    Added generate button, logic to generate squares to color on press

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
+// Handles button press to generate grid
+const button = document.querySelector("#generate");
+
+button.addEventListener("click", generateGrid);
+
+// Generates squares to be colored in by fuction colorBox
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 257; i++) {
-const gridbox = document.createElement("div");
-gridbox.classList.add("gridbox");
+function generateGrid() {
+    for (let i = 0; i < 257; i++) {
+        console.log("Grid is being generated!");
+        const gridbox = document.createElement("div");
+        gridbox.classList.add("gridbox");
 
-container.appendChild(gridbox);
+        container.appendChild(gridbox);
+    }
+
+    // Listens for mouseover event to feed into colorBox
+    const boxes = document.querySelectorAll(".gridbox");
+    boxes.forEach((gridbox) => {
+        gridbox.addEventListener("mouseover", colorBox);
+    })
 }
 
+// Colors squares when moused over
 function colorBox(e) {
     console.log("Moused over!");
     e.target.setAttribute("style", "background: black");
 }
 
-const boxes = document.querySelectorAll(".gridbox");
-
-boxes.forEach((gridbox) => {
-    gridbox.addEventListener("mouseover", colorBox);
-})
 
 function testAlert(e) {
     console.log(e.target);
