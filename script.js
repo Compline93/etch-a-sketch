814935dfdcd27f792a4c7d30a03d144a0fe30dc4
Compline93 <97375682+Compline93@users.noreply.github.com>
2024-05-29 10:37:33 -0400

    Established ability to clear grid and create new one via generate grid button

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 // Handles button press to generate grid
 const button = document.querySelector("#generate");
 
-button.addEventListener("click", generateGrid);
+button.addEventListener("click", initializeGrid);
 
 // Generates squares to be colored in by fuction colorBox
 const container = document.querySelector("#container");
 
-function generateGrid() {
-    for (let i = 0; i < 257; i++) {
+function createBox() {
+    const gridbox = document.createElement("div");
+    gridbox.classList.add("gridbox");
+
+    container.appendChild(gridbox);
+}
+
+// Creates a new grid for boxes
+function generateNewGrid() {
+    for (let i = 0; i < 256; i++) {
         console.log("Grid is being generated!");
-        const gridbox = document.createElement("div");
-        gridbox.classList.add("gridbox");
-
-        container.appendChild(gridbox);
+        createBox();
     }
+}
 
-    // Listens for mouseover event to feed into colorBox
+// Listens for mouseover event to feed into colorBox
+function initMouseOver() {
     const boxes = document.querySelectorAll(".gridbox");
     boxes.forEach((gridbox) => {
         gridbox.addEventListener("mouseover", colorBox);
@@ -26,6 +33,19 @@ function generateGrid() {
 function colorBox(e) {
     console.log("Moused over!");
     e.target.setAttribute("style", "background: black");
+}
+
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+// Initializes sketchpad
+function initializeGrid() {
+    clearGrid();
+    generateNewGrid();
+    initMouseOver();
 }
 
 
