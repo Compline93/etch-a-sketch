489f6c87674a70cae0baab20733aaa20d097b100
Compline93 <97375682+Compline93@users.noreply.github.com>
2024-05-29 08:04:10 -0400

    Established logic for coloring gridbox on mouseover

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ gridbox.classList.add("gridbox");
 
 container.appendChild(gridbox);
 }
+
+function colorBox(e) {
+    console.log("Moused over!");
+    e.target.setAttribute("style", "background: black");
+}
+
+const boxes = document.querySelectorAll(".gridbox");
+
+boxes.forEach((gridbox) => {
+    gridbox.addEventListener("mouseover", colorBox);
+})
+
+function testAlert(e) {
+    console.log(e.target);
+}
